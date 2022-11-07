dd3932072889115cdd8b7c56989f08d7a529fc99
Balaganesh K <89638496+Balaganesh003@users.noreply.github.com>
2022-11-08 05:11:06 +0530

    Create main.cpp

---
+++ oops/templates/3/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+template <class Celebration>
+Celebration Rome(Celebration a,Celebration b,Celebration c){
+ cout<<((b+c-1)/c)*((a+c-1)/c);
+ return 1;
+}
+int main()
+{
+ int a,b,c;
+ cin>>a>>b>>c;
+ Rome(a,b,c);
+return 0;
+}

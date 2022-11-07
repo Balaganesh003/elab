5e05951b223f80b49e5e4caaba67e61405ac02ec
Balaganesh K <89638496+Balaganesh003@users.noreply.github.com>
2022-11-08 05:09:37 +0530

    Create main.cpp

---
+++ oops/templates/2/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+template <class Paper>
+Paper Square(Paper T){
+ if(T%2==0)
+ return 4*T+1;
+ else if(T%4==1)
+ return 2*T+1;
+ else
+ return T+1;
+}
+int main()
+{
+ int T,n;
+ cin>>T;
+ while(T--){
+ cin>>n;
+ cout<<Square(n)<<endl;
+ }
+return 0;
+}

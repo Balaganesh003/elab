b2b2beff78fd58cb37a7301b872b153a476bf123
Balaganesh K <89638496+Balaganesh003@users.noreply.github.com>
2022-11-08 04:59:54 +0530

    Create main.cpp

---
+++ oops/io operations/2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+float num1,num2;
+double resnum1,resnum2;
+cin >> num1 >> num2;
+resnum1 = (double) num1;
+resnum2 = (double) num2;
+std::cout << std::setprecision(6) << std::fixed;
+std::cout<< resnum1<< "\n"<< resnum2 ;
+
+	return 0;
+}

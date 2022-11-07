36812671ef424e6468c42857666542398be563a4
Balaganesh K <89638496+Balaganesh003@users.noreply.github.com>
2022-11-08 05:18:44 +0530

    Create main.cpp

---
+++ oops/abstract class & virtual functions/2/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class School{
+ public:
+ int roll;
+ string name;
+ virtual void getdata(){};
+ virtual void display(){};
+};
+class District : public School{
+ void getdata();
+ void display();
+};
+void District :: getdata(){
+ cin>>roll>>name;
+ }
+void District :: display(){
+ cout<<"Student Name is: "<<name<<endl<<"Student Roll no is: "<<roll;
+ }
+int main()
+{
+ District obj;
+ School* ptr;
+ ptr = &obj;
+ ptr -> getdata();
+ ptr -> display();
+return 0;
+}

24cfaaed366fba2f3c9997967190db2628b4ca42
Balaganesh K <89638496+Balaganesh003@users.noreply.github.com>
2022-11-08 06:15:17 +0530

    Create main.cpp

---
+++ dsa/sorting/2/main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,i,sum=0;
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+        cin>>arr[i];
+    sort(arr,arr+n);
+    for(i=0;i<n;i++)
+    {
+        int z= arr[n-i-1]-(i+1);
+        sum=sum+abs(z);
+    }
+    cout<<sum;
+    return 0;
+    cout<<"swap(l,r);";
+}

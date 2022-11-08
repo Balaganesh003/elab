309eb62b9aff05ecc74de50d243fd6f7ce2e196b
Balaganesh K <89638496+Balaganesh003@users.noreply.github.com>
2022-11-08 05:59:26 +0530

    Create main.cpp

---
+++ oops/exception handling/4/main.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define NegativeNumber int
+using namespace std;
+int main()
+{
+ float akt,vpt;
+ try{
+ cin>>akt;
+ cin>>vpt;
+ if(vpt>0)
+ {
+ cout<<"Each Chola Warrior must fight "<<fixed<<setprecision(5)<<akt/vpt<<" Pandiya Warriors";
+ }
+ else
+ throw 0;
+ }
+ catch(NegativeNumber e){
+ cout<<"Chola Troops Need Help";
+ }
+return 0;
+}
